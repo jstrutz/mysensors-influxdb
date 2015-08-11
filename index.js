d4a8749b37a94f42656e8de2f985b0dbba0da84f
Jason Strutz <j@jasonstrutz.com>
2015-08-11 09:38:37 -0700

    Fix catches for promises

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var defer = require('p-promise').defer,
+var P = require('p-promise'),
     SerialPort = require('serialport'),
     Mouth = require('cowboymouth'),
     Sensors = require('sensors');
 
 var getGatewaySerialPort = function() {
-  var deferred = defer();
+  var deferred = P.defer();
   SerialPort.list(function (err, ports) {
+    console.log(ports);
     if (err) {
       deferred.reject(err);
     }
@@ -16,13 +17,15 @@ var getGatewaySerialPort = function() {
           deferred.resolve(portname);
         }
       }
+      deferred.reject("No gateways found");
     }
   });
   return deferred.promise;
+  // return Promise.reject();
 }
 
 var openSerialPort = function(portname, baudrate) {
-  var deferred = defer();
+  var deferred = P.defer();
 
   var serialPort = new SerialPort.SerialPort(portname, {
     parser: SerialPort.parsers.readline("\n"),
@@ -47,59 +50,62 @@ var openSerialPort = function(portname, baudrate) {
 
 // var mouth = new Mouth(stream);
 
-getGatewaySerialPort().then(function(portname) {
-  console.log('found controller at', portname);
-  return openSerialPort(portname, 115200);
-}).then(function(serialPort) {
-  var mouth = new Mouth(serialPort);
+getGatewaySerialPort()
+  .then(function(portname) {
+    console.log('found controller at', portname);
+    return openSerialPort(portname, 115200);
+  })
+  .then(function(serialPort) {
+    var mouth = new Mouth(serialPort);
 
-  // serialPort.on('data', function(data) {
-  //   console.log('raw serial data: ', data, Sensors.parse(data));
-  // });
-  // var mouthEvents = ['addon', 'log', 'name', 'version', 'battery', 'protocol', 'reading'];
-  //
-  // for (var i=0; i<mouthEvents.length; i++) {
-  //   var eventName = mouthEvents[i];
-  //   mouth.on(eventName, function(data) {
-  //     var formattedData = { type: data.type, value: data.value, time: data.time };
-  //     console.log(eventName, data, formattedData);
-  //   });
-  // }
-  //
-  var events = ['battery', 'name', 'version'];
-  events.forEach(function (event) {
+    // serialPort.on('data', function(data) {
+    //   console.log('raw serial data: ', data, Sensors.parse(data));
+    // });
+    // var mouthEvents = ['addon', 'log', 'name', 'version', 'battery', 'protocol', 'reading'];
+    //
+    // for (var i=0; i<mouthEvents.length; i++) {
+    //   var eventName = mouthEvents[i];
+    //   mouth.on(eventName, function(data) {
+    //     var formattedData = { type: data.type, value: data.value, time: data.time };
+    //     console.log(eventName, data, formattedData);
+    //   });
+    // }
+    //
+    var events = ['battery', 'name', 'version'];
+    events.forEach(function (event) {
 
-      mouth.on(event, function (data) {
-          console.log("got event", event, data);
-          // jenny.updateBoard(data.boardId, data, internals.handleError);
-      });
-  });
+        mouth.on(event, function (data) {
+            console.log("got event", event, data);
+            // jenny.updateBoard(data.boardId, data, internals.handleError);
+        });
+    });
 
-  mouth.on('reading', function (data) {
-      var formattedData = { type: data.type, value: data.value, time: data.time };
-      // jenny.createReading(data.boardId, data.addonId, formattedData, internals.handleError);
-      console.log('reading', data.boardId, data.addonId, formattedData);
-  });
+    mouth.on('reading', function (data) {
+        var formattedData = { type: data.type, value: data.value, time: data.time };
+        // jenny.createReading(data.boardId, data.addonId, formattedData, internals.handleError);
+        console.log('reading', data.boardId, data.addonId, formattedData);
+    });
 
-  mouth.on('addon', function (data) {
-    console.log('addon', data);
-      // jenny.updateAddon(data.boardId, data.addonId, { type: data.type }, internals.handleError);
-  });
+    mouth.on('addon', function (data) {
+      console.log('addon', data);
+        // jenny.updateAddon(data.boardId, data.addonId, { type: data.type }, internals.handleError);
+    });
 
-  mouth.on('log', function (message) {
-    console.log('log', message);
-      // jenny.log(message, internals.handleError);
-  });
-  mouth.on('register', function (message) {
-    console.log('ID request');
-    mouth.writeId(101);
-  });
+    mouth.on('log', function (message) {
+      console.log('log', message);
+        // jenny.log(message, internals.handleError);
+    });
+    mouth.on('register', function (message) {
+      console.log('ID request');
+      mouth.writeId(101);
+    });
 
-  mouth.on('error', function(err) {
+    mouth.on('error', function(err) {
+      console.error(err);
+    });
+
+    console.log('watching events');
+  })
+  .fail(function(err) {
     console.error(err);
   });
-
-  console.log('watching events');
-}).catch(function(err) {
-  console.error(err);
-});
